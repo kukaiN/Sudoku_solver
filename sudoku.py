e596f42d1d5874a45ea62c764a9e1f317213d8fa
kukaiN <kukainakahata@gmail.com>
2020-02-12 18:12:30 -0500

    Create sudoku.py

---
+++ sudoku.py
@@ -0,0 +1,28 @@
+from math import sqrt
+
+
+
+
+
+def main():
+
+
+def solve_sudoku():
+
+
+
+def create_sudoku__board(x, y):
+    if x == y and sqrt(x)
+
+
+
+
+
+
+
+
+
+
+
+if "__name__" == "__main__":
+    main()
